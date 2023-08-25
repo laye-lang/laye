a23e4c473ee044045a040bccc0ae0d8c8ae434e7
nashiora <22553694+nashiora@users.noreply.github.com>
2023-08-25 19:46:46 -0400

    [laye] Get bare-minimum lexing done for Laye

--- include/stdio.h
+++ include/stdio.h
@@ -12,14 +12,6 @@ typedef unsigned long long size_t;
 #define SEEK_CUR  (1)
 #define SEEK_END  (2)
 
-// NOTE(local): this kind of thing is going to be the hardest to port over without just copying parts from actual std library headers
-#ifdef WIN32
-FILE* __cdecl __acrt_iob_func(unsigned _Ix);
-#  define stdin  (__acrt_iob_func(0))
-#  define stdout (__acrt_iob_func(1))
-#  define stderr (__acrt_iob_func(2))
-#endif
-
 int printf(const char* format, ...);
 
 FILE* fopen(const char* file_name, const char* mode);
