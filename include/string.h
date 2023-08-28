d5b593e65f9d8a847378a80158e9d9e7273b9c9e
nashiora <22553694+nashiora@users.noreply.github.com>
2023-08-28 18:46:17 -0400

    [layec] Switch memset declaration on platform

--- include/string.h
+++ include/string.h
@@ -5,6 +5,10 @@ int strcmp(const char* a, const char* b);
 int strncmp(const char* a, const char* b, unsigned long long n);
 unsigned long long strlen(const char* s);
 void* memcpy(void* dest, const void* source, unsigned long long count);
+#ifdef WIN32
+void* memset(void* dest, int value, unsigned long long count);
+#else
 void* memset(void* dest, int value, unsigned long count);
+#endif
 
 #endif // LAYEC_LIBC_STRING_H
