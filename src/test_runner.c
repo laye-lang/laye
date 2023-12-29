d425eacaad7d1631c1fac34e2977c67dc0377f8a
Local Atticus <contact@nashiora.com>
2023-12-29 00:01:50 -0500

    [Tests] Relocate test files and update test runner

--- src/test_runner.c
+++ src/test_runner.c
@@ -17,44 +17,24 @@
 
 typedef struct test_state {
     int test_count;
-    int num_tests_failed;
+    dynarr(const char*) failed_tests;
 } test_state;
 
 static bool ensure_fchk_installed(void);
 static bool cstring_ends_with(const char* s, const char* ending);
 static bool run_test(const char* test_file_path);
+static void run_tests_in_directory(test_state* state, const char* test_directory, const char* extension);
 
 int main(int argc, char** argv) {
     if (!ensure_fchk_installed()) {
         return 1;
     }
 
-    Nob_File_Paths test_file_paths = {};
-    if (!nob_read_entire_dir("./test", &test_file_paths)) {
-        nob_log(NOB_ERROR, "Failed to enumerate test directory");
-        return 1;
-    }
-
     test_state state = {};
+    run_tests_in_directory(&state, "./test/laye", ".laye");
 
-    dynarr(const char*) failed_tests = NULL;
-    for (size_t i = 0; i < test_file_paths.count; i++) {
-        const char* test_file_path = test_file_paths.items[i];
-        if (!cstring_ends_with(test_file_path, ".laye")) {
-            continue;
-        }
-
-        const char* test_full_name = nob_temp_sprintf("./test/%s", test_file_path);
-        state.test_count++;
-
-        bool test_success = run_test(test_full_name);
-        if (!test_success) {
-            arr_push(failed_tests, test_full_name);
-            state.num_tests_failed++;
-        }
-    }
-
-    if (state.num_tests_failed == 0) {
+    int64_t num_tests_failed = arr_count(state.failed_tests);
+    if (num_tests_failed == 0) {
         fprintf(
             stderr,
             "\n%s100%% of tests passed%s out of %d.\n",
@@ -63,28 +43,52 @@ int main(int argc, char** argv) {
             state.test_count
         );
     } else {
-        int percent_success = (state.test_count - state.num_tests_failed) * 100 / state.test_count;
+        int percent_success = (state.test_count - num_tests_failed) * 100 / state.test_count;
         fprintf(
             stderr,
             "\n%d%% of tests passed, %s%d tests failed%s out of %d.\n",
             percent_success,
             ANSI_COLOR_RED,
-            state.num_tests_failed,
+            (int)num_tests_failed,
             ANSI_COLOR_RESET,
             state.test_count
         );
 
         fprintf(stderr, "\nThe following tests FAILED:\n");
-        for (int64_t i = 0, count = arr_count(failed_tests); i < count; i++) {
-            fprintf(stderr, "\t%s%s (Failed)%s\n", ANSI_COLOR_RED, failed_tests[i], ANSI_COLOR_RESET);
+        for (int64_t i = 0; i < num_tests_failed; i++) {
+            fprintf(stderr, "\t%s%s (Failed)%s\n", ANSI_COLOR_RED, state.failed_tests[i], ANSI_COLOR_RESET);
         }
     }
 
-    arr_free(failed_tests);
-    nob_da_free(test_file_paths);
+    arr_free(state.failed_tests);
     nob_temp_reset();
 
     return 0;
+}
+
+static void run_tests_in_directory(test_state* state, const char* test_directory, const char* extension) {
+    Nob_File_Paths test_file_paths = {};
+    if (!nob_read_entire_dir(test_directory, &test_file_paths)) {
+        nob_log(NOB_ERROR, "Failed to enumerate test directory");
+        return;
+    }
+
+    for (size_t i = 0; i < test_file_paths.count; i++) {
+        const char* test_file_path = test_file_paths.items[i];
+        if (!cstring_ends_with(test_file_path, extension)) {
+            continue;
+        }
+
+        const char* test_full_name = nob_temp_sprintf("%s/%s", test_directory, test_file_path);
+        state->test_count++;
+
+        bool test_success = run_test(test_full_name);
+        if (!test_success) {
+            arr_push(state->failed_tests, test_full_name);
+        }
+    }
+
+    nob_da_free(test_file_paths);
 }
 
 static bool cstring_ends_with(const char* s, const char* ending) {
